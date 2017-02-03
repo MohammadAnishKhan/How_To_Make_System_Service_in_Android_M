f7958b3973befb174e003063c6819546747dfe95
MohammadAnishKhan <mohammadanish802.11@gmail.com>
2017-02-03 09:47:01 +0530

    Update Context.java

--- frameworks/base/core/java/android/content/Context.java
+++ frameworks/base/core/java/android/content/Context.java
@@ -1,1 +1,14 @@
 //Define SystemService name in frameworks/base/core/java/android/content/Context.java
+--- a/core/java/android/content/Context.java
++++ b/core/java/android/content/Context.java
+@@ -2958,6 +2958,14 @@ public abstract class Context {
+      * @hide
+      */
+
++    /**
++* Use with {@link #getSystemService} to retrieve a {@link
++* android.manandaidl.MySampleSystemServiceManager} instance
++* @see #getSystemService
++* @see android.manandaidl.MySampleSystemServiceManager
++*/
++public static final String MY_SYSTEM_SERVICE = "my";
